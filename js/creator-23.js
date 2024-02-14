98fc513c89007ee72735ef0605e670f59199a0f5
sentixxx <1091441107@qq.com>
2024-02-14 21:18:13 +0800

    增加了中文字体支持 {fontCStitle} {fontCStext} {fontCSflavor}

--- js/creator-23.js
+++ js/creator-23.js
@@ -3393,9 +3393,9 @@ function writeText(textObject, targetContext) {
 		rawText = params.get('copyright'); //so people using CC for custom card games without WotC's IP can customize their copyright info
 		if (rawText == 'none') { rawText = ''; }
 	}
-	if (rawText.toLowerCase().includes('{cardname}') || rawText.toLowerCase().includes('~')) {
-		rawText = rawText.replace(/{cardname}|~/ig, getInlineCardName());
-	}
+	// if (rawText.toLowerCase().includes('{cardname}') || rawText.toLowerCase().includes('~')) {
+	// 	rawText = rawText.replace(/{cardname}|~/ig, getInlineCardName());
+	// }
 	if (document.querySelector('#info-artist').value == '') {
 		rawText = rawText.replace('\uFFEE{elemidinfo-artist}', '');
 	}
