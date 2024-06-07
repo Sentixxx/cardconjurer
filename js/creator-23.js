41b11bcaaa1032f51974511e772302adab596ee6
sentixxx <1091441107@qq.com>
2024-06-07 15:15:12 +0800

    fix devoid

--- js/creator-23.js
+++ js/creator-23.js
@@ -830,19 +830,19 @@ function autoFrame() {
 			}
 		});
 		//console.log("pre:" + colors);
-		if (!colors.includes('W') && (rules.toLowerCase().includes('plains') || card.text.type.text.toLowerCase().includes('plains') || rules.toLowerCase().includes('平原') || card.text.type.text.toLowerCase().includes('平原'))) {
+		if (!colors.includes('W') && (rules.toLowerCase().includes('plains') || types.includes('plains') || rules.toLowerCase().includes('平原') || types.includes('平原'))) {
 			colors.push('W');
 		}
-		if (!colors.includes('U') && (rules.toLowerCase().includes('island') || card.text.type.text.toLowerCase().includes('island') || rules.toLowerCase().includes('海岛') || card.text.type.text.toLowerCase().includes('海岛'))) {
+		if (!colors.includes('U') && (rules.toLowerCase().includes('island') || types.includes('island') || rules.toLowerCase().includes('海岛') || types.includes('海岛'))) {
 			colors.push('U');
 		}
-		if (!colors.includes('B') && (rules.toLowerCase().includes('swamp') || card.text.type.text.toLowerCase().includes('swamp') || rules.toLowerCase().includes('沼泽') || card.text.type.text.toLowerCase().includes('沼泽'))) {
+		if (!colors.includes('B') && (rules.toLowerCase().includes('swamp') || types.includes('swamp') || rules.toLowerCase().includes('沼泽') || types.includes('沼泽'))) {
 			colors.push('B');
 		}
-		if (!colors.includes('R') && (rules.toLowerCase().includes('mountain') || card.text.type.text.toLowerCase().includes('mountain') || rules.toLowerCase().includes('山脉') || card.text.type.text.toLowerCase().includes('山脉'))) {
+		if (!colors.includes('R') && (rules.toLowerCase().includes('mountain') || types.includes('mountain') || rules.toLowerCase().includes('山脉') || types.includes('山脉'))) {
 			colors.push('R');
 		}
-		if (!colors.includes('G') && (rules.toLowerCase().includes('forest') || card.text.type.text.toLowerCase().includes('forest') || rules.toLowerCase().includes('树林') || card.text.type.text.toLowerCase().includes('树林'))) {
+		if (!colors.includes('G') && (rules.toLowerCase().includes('forest') || types.includes('forest') || rules.toLowerCase().includes('树林') || types.includes('树林'))) {
 			colors.push('G');
 		}
 		// console.log("color:" + colors);
@@ -867,8 +867,8 @@ function autoFrame() {
 
 	} else {
 		colors = [...new Set(card.text.mana.text.toUpperCase().split('').filter(char => ['W', 'U', 'B', 'R', 'G'].includes(char)))];
-		if(rules.includes('devoid')) {
-			colors.push('D');
+		if(rules.toLowerCase().includes('devoid') || rules.toLowerCase().includes('虚色')) {
+			colors = [];
 		}
 	}
 	
