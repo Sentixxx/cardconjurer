0897fe0387e6790b4f083e862fe0efea26abc3e6
sentuix <1091441107@qq.com>
2025-02-09 17:23:26 +0800

    [CN]Add Chinese translation

--- data/scripts/footer.js
+++ data/scripts/footer.js
@@ -30,22 +30,16 @@ document.getElementById('footer').innerHTML = `
         <div>Navigation</div>
         <hr>
         <div>
-            <a href='/'>Home</a><br>
-            <a href='/creator'>New Card Creator</a><br>
+            <a href='/'>主页</a><br>
+            <a href='/creator'>制卡</a><br>
         </div>
     </div>
     <div>
+
         <div>Legal</div>
         <hr>
         <div>
-            <a href='/legal'>Terms and Conditions</a><br>
-        </div>
-    </div>
-    <div>
-        <div>Contact</div>
-        <hr>
-        <div>
-            <label class="truncate"><a href="mailto:CardConjurerMTG@gmail.com?subject=Card Conjurer" target="_blank">CardConjurerMTG@gmail.com</a></label>
+            <a href='/legal'>条款和条件</a><br>
         </div>
     </div>
     <script defer src="/data/scripts/animations.js"></script>
